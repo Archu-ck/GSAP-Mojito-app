d7d699b84242fc30164f2a3dbe61ee40e923e69d
Archu-ck <archismohakud5@gmail.com>
2025-09-26 20:51:49 +0530

    Implemented the contact section and finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
 import Menu from "./components/Menu";
+import Contact from "./components/Contact";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <main>
+    <main className="overflow-y-auto">
         <Navbar />
         <Hero />
         <Cocktails />
         <About />
         <Art />
         <Menu />
+        <Contact />
 
     </main>
   )
